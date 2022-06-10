122d00bc1a6a8ce180e6e0a522923e5a532d8af5
ljp <ljp@kalijp.kalijp.zone>
2022-06-10 23:21:21 +0800

    artists done..

--- hanart.py
+++ hanart.py
@@ -7,6 +7,16 @@ http://data.discogs.com/?prefix=data/2022/
  freeRelease (id, notes,formats,images,artists, labels,genres,styles,released,country)
 '''
 import xml.etree.ElementTree as ET
+import pymysql
+
+conn = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="sa",
+    database="upwork",
+    charset="utf8")
+
+cursor = conn.cursor()
  
 tree = ET.parse("artsmall.xml")
 # 根节点
@@ -17,8 +27,6 @@ print('root_tag:',root.tag)
 artlist = []
 for art in root:
     print("   ",art.tag)
-    # artdata = []
-    
     artdic = {"id":"", "name":"","realname":"","profile":"","data_quality":"","images":"","namevariations":"","aliases":"","members":"","urls":""}
     for artag in art:
         #print("        ",artag.tag,":",artag.text)
@@ -29,20 +37,51 @@ for art in root:
         elif artag.tag == 'realname':
             artdic['realname'] = artag.text
         elif artag.tag == 'profile':
-            artdic['pfofile'] = artag.text
+            artdic['profile'] = artag.text
         elif artag.tag == 'data_quality':
             artdic['data_quality'] = artag.text
-
-        
-        #print("        ",artag.tag,":",artag.text,",",type(artag.text))
-        if artag.text is not None:
-            print("        ",artag.tag,":",artag.text,",",len(artag.text))
+        elif artag.tag == 'namevariations':
             data2 = []
             for arrtag in artag:
                 data2.append(arrtag.text)
-           # print("            ",arrtag.tag,":",arrtag.text)
-            print(data2)
-    artlist.append(artdic) 
-    #print(artdic)
+            artdic['namevariations'] = data2
+        elif artag.tag == 'aliases':
+            data2 = []
+            for arrtag in artag:
+                data2.append(arrtag.text)
+            artdic['aliases'] = data2
+        elif artag.tag == 'members':
+            data2 = []
+            for arrtag in artag:
+                data2.append(arrtag.text)
+            artdic['members'] = data2
+        elif artag.tag == 'urls':
+            data2 = []
+            for arrtag in artag:
+                data2.append(arrtag.text)
+            artdic['urls'] = data2
+        elif artag.text is None:
+            pass
+ 
 
-print(artlist)
+        
+        #print("        ",artag.tag,":",artag.text,",",type(artag.text))
+       #  if artag.text is  None:
+
+           # print("        ",artag.tag,":",artag.text,",",len(artag.text))
+           # data2 = []
+           # for arrtag in artag:
+           #     data2.append(arrtag.text)
+           # print("            ",arrtag.tag,":",arrtag.text)
+            # print(data2)
+    artlist.append(artdic) 
+    #sql = 'insert into freeArtistsMeta(id,namevariations) values (%s,%s);'
+    sql = 'insert into freeArtistsMeta (id, name,realname,data_quality,images,namevariations,aliases,members,urls,profile) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+    cursor.execute(sql,(artdic['id'],artdic['name'],artdic['realname'],artdic['data_quality'],artdic['images'],str(artdic['namevariations']),str(artdic['aliases']),str(artdic['members']),str(artdic['urls']),artdic['profile']))
+    conn.commit()
+    print(artdic)
+
+cursor.close()
+conn.close() 
+
+#print(artlist)
